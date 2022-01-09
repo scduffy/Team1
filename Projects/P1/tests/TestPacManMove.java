8c80ef7edb7de9b677bc1aa7c277f23f67b2408b
Samikshya Sapkota <ssapkota@terpmail.umd.edu>
2022-01-09 14:13:59 -0500

    Revert "Merge pull request #34 from cmsc388T-winter22/PacMan-is_ghost_in_range"
    This reverts commit 3470fba0f9c3a080fe08e5478d30765fb2c84c2e, reversing
    changes made to 04921d1e4662b80daf8d253cc13790414e524bf2.

--- Projects/P1/tests/TestPacManMove.java
+++ Projects/P1/tests/TestPacManMove.java
@@ -5,13 +5,6 @@ import java.io.*;
 public class TestPacManMove extends TestCase {
 
 	public void testPacManMove() throws FileNotFoundException{
-		//Creating A Map
-		Mainframe frame = new MainFrame(); //Creates A New Map With Walls and Tokens Initialized
-		
-		//Creating Players
-		PacMan pacman = frame.addPacMan(new Location(1, 1)); //Creates PacMan at location x, y
-		assertTrue(pacman.move());
-		pacman = frame.addPacMan(new Location(0, 0));
-		assertFalse(pacman.move());
+		return null;
 	}
 }
