19e9b75d1633faa3518341109cd212f2e4d298d8
Elizabeth Bernhardt <ebernhar@terpmail.umd.edu>
2022-01-09 15:51:02 -0500

    Edited test for eatCookie(String name) in Map class

--- Projects/P1/tests/TestMapEatCookie.java
+++ Projects/P1/tests/TestMapEatCookie.java
@@ -10,7 +10,10 @@ public class TestMapEatCookie extends TestCase {
 
 		PacMan pacman = frame.addPacMan(new Location(1, 1));
 		
+		assertTrue(map.getCookies() == 0);
 		assertTrue(map.eatCookie("pacman") != null);
+		assertTrue(map.getCookies() == 1);
 		assertTrue(map.eatCookie("pacman") == null); // Cookie at (1,1) already eaten
+		assertTrue(map.getCookies() == 1); // No cookie consumed by second call to eatCookie
 	}
 }
