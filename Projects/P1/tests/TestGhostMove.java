dd4dcc7560534424f65de7f99349109f45662df9
mikeo9911 <70450197+mikeo9911@users.noreply.github.com>
2022-01-09 14:19:12 -0500

    Made small fix to testGhostMove per Sami

--- Projects/P1/tests/TestGhostMove.java
+++ Projects/P1/tests/TestGhostMove.java
@@ -5,6 +5,13 @@ import java.io.*;
 public class TestGhostMove extends TestCase {
 
 	public void testGhostMove() throws FileNotFoundException{
-	
+		//Creating A Map
+		MainFrame frame = new MainFrame(); //Creates A New Map With Walls and Tokens Initialized
+
+		//Creating Players
+		Ghost ghost = frame.addGhost(new Location(2, 1), "stinky", Color.red);
+		assertTrue(ghost.move());
+		ghost = frame.addPacMan(new Location(0, 0));
+		assertFalse(ghost.move());
 	}
 }
