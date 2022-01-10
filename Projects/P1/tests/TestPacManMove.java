841af662e61d7eb9f01a16d0a242a72ba043e061
AnthonyLiu-Git <anthonyliu2013@gmail.com>
2022-01-10 13:39:08 -0500

    Revert Pacman Move test

--- Projects/P1/tests/TestPacManMove.java
+++ Projects/P1/tests/TestPacManMove.java
@@ -4,14 +4,7 @@ import java.io.*;
 
 public class TestPacManMove extends TestCase {
 
-	public void testPacManMove() throws FileNotFoundException{
-		//Creating A Map
-		Mainframe frame = new MainFrame(); //Creates A New Map With Walls and Tokens Initialized
-
-		//Creating Players
-		Ghost ghost = frame.addGhost(new Location(2, 1), "stinky", Color.red);
-		assertTrue(ghost.move());
-		ghost = frame.addPacMan(new Location(0, 0));
-		assertFalse(ghost.move());
+	public void testPacManMove() throws FileNotFoundException {
+		return null;
 	}
 }
