c454797012fa32f41b9cae87a67e2ed841900346
Samikshya Sapkota <ssapkota@terpmail.umd.edu>
2022-01-10 01:37:31 -0500

    Updated ghost attack (removed diagonal move)

--- Projects/P1/src/Ghost.java
+++ Projects/P1/src/Ghost.java
@@ -51,19 +51,25 @@ public class Ghost{
 	}
 
 	public boolean attack() {
-		if (is_pacman_in_range()) {
-			ArrayList<Location> valid_moves = get_valid_moves();
-			for(Location move : valid_moves) {
-				if(myMap.getLoc(move).contains(Map.Type.PACMAN)) {
-					boolean attacked = myMap.attack(this.myName);
-					if(attacked) {
-						myLoc = move;
-					}
-					return attacked;
-				}
-			}
+	// center check
+	if(myMap.getLoc(myLoc).contains(Map.Type.GHOST)) {			return true;
+		}
+		// West check
+		if(myMap.getLoc(new Location(myLoc.x - 1, myLoc.y)).contains(Map.Type.GHOST)) {
+			return true;
+		}
+		// East check
+		if(myMap.getLoc(new Location(myLoc.x + 1, myLoc.y)).contains(Map.Type.GHOST)) {
+			return true;
+		}
+		// North check
+		if(myMap.getLoc(new Location(myLoc.x, myLoc.y + 1)).contains(Map.Type.GHOST)) {
+			return true;
+		}
+		// South check
+		if(myMap.getLoc(new Location(myLoc.x, myLoc.y - 1)).contains(Map.Type.GHOST)) {
+			return true;
 		}
 		return false;
-	}
-	}
+	}	
 }
