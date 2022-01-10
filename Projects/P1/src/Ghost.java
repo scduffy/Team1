80c168cc1b0f0ced119a1ad2e7c25ee026577fa2
Samikshya Sapkota <ssapkota@terpmail.umd.edu>
2022-01-10 10:33:53 -0500

    Added Ghost attack

--- Projects/P1/src/Ghost.java
+++ Projects/P1/src/Ghost.java
@@ -70,6 +70,7 @@ public class Ghost{
 		if(myMap.getLoc(new Location(myLoc.x, myLoc.y - 1)).contains(Map.Type.GHOST)) {
 			return true;
 		}
+
 		return false;
 	}	
 }
--- Projects/P1/tests/TestAttack.java
+++ Projects/P1/tests/TestAttack.java
@@ -5,6 +5,14 @@ import java.io.*;
 public class TestAttack extends TestCase {
 
 	public void testAttack() throws FileNotFoundException{
-		return null;	
-	}
+		MainFrame frame = new MainFrame();
+
+		Ghost ghost1 = frame.addGhost(new Location(1, 1), "ghost1", Color.red);
+		Ghost ghost2 = frame.addGhost(new Location(5, 9), "ghost2", Color.blue);
+
+		PacMan pacman = frame.addPacMan(new Location(6, 9));
+		assertTrue(ghost1.attack() == false);
+		assertTrue(ghost2.attack());
+	}	
+
 }
