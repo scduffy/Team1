cdd1af336b84a2a13247ea664f7fbc7be8ba9439
mikeo9911 <70450197+mikeo9911@users.noreply.github.com>
2022-01-10 10:03:53 -0500

    Revert "Pacman test is ghost in range"

--- Projects/P1/tests/TestConsume.java
+++ Projects/P1/tests/TestConsume.java
@@ -5,10 +5,6 @@ import java.io.*;
 public class TestConsume extends TestCase {
 
 	public void testConsume() throws FileNotFoundException {
-		MainFrame frame = new MainFrame();
-                
-                PacMan pacman = frame.addPacMan(new Location(1, 1));
-                assertTrue(pacman.consume() != null);
-                assertTrue(pacman.consume() == null); // Cookie already consumed
+		return null;	
 	}
 }
