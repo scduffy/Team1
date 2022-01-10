773b5e7ddd0b46b3df338fa2c12876d68f603b9c
AnthonyLiu-Git <anthonyliu2013@gmail.com>
2022-01-10 10:11:59 -0500

    Revert "Added test for eatCookie() method in Map class"
    This reverts commit ca4dcdd901f0fc830a600d873d0e982e571232f5.

--- Projects/P1/tests/TestMapEatCookie.java
+++ Projects/P1/tests/TestMapEatCookie.java
@@ -2,15 +2,9 @@ import junit.framework.*;
 import java.awt.Color;
 import java.io.*;
 
-public class TestMapEatCookie extends TestCase {
+public class TestMapEatCookie {
 	
-	public void testMapEatCookie() throws FileNotFoundException{
-		MainFrame frame = new MainFrame();
-		Map map = frame.getMap();
-
-		PacMan pacman = frame.addPacMan(new Location(1, 1));
-		
-		assertTrue(map.eatCookie("pacman") != null);
-		assertTrue(map.eatCookie("pacman") == null); // Cookie at (1,1) already eaten
+	public void testMapEatCookie() {
+		return null;
 	}
 }
