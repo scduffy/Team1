2cfa75757eeefb05eb35a1275bec5f6522cf4994
AnthonyLiu-Git <anthonyliu2013@gmail.com>
2022-01-10 14:27:45 -0500

    Revert "Merge branch 'main' into FTR-ghost"
    This reverts commit 6aa3eb8f4e61c458da30e1d7fdab62c23e61e6f2, reversing
    changes made to fb9b0e22e4338113c993c182e13941eaa21a9b82.

--- Projects/P1/tests/TestMapEatCookie.java
+++ Projects/P1/tests/TestMapEatCookie.java
@@ -2,18 +2,9 @@ import junit.framework.*;
 import java.awt.Color;
 import java.io.*;
 
-public class TestMapEatCookie extends TestCase {
+public class TestMapEatCookie {
 	
-	public void testMapEatCookie() throws FileNotFoundException{
-		MainFrame frame = new MainFrame();
-		Map map = frame.getMap();
-
-		PacMan pacman = frame.addPacMan(new Location(1, 1));
-		
-		assertTrue(map.getCookies() == 0);
-		assertTrue(map.eatCookie("pacman") != null);
-		assertTrue(map.getCookies() == 1);
-		assertTrue(map.eatCookie("pacman") == null); // Cookie at (1,1) already eaten
-		assertTrue(map.getCookies() == 1); // No cookie consumed by second call to eatCookie
+	public void testMapEatCookie() {
+		return null;
 	}
 }
--- Projects/P1/tests/TestMapMove.java
+++ Projects/P1/tests/TestMapMove.java
@@ -5,14 +5,6 @@ import java.io.*;
 public class TestMapMove {
 
 	public void testMapMove() {
-		MainFrame frame = new MainFrame();
-
-		Ghost ghost1 = frame.addGhost(new Location(1, 1), "ghost1", Color.red);
-		Ghost ghost2 = frame.addGhost(new Location(5, 9), "ghost2", Color.blue);
-
-		PacMan pacman = frame.addPacMan(new Location(6, 9));
-		assertTrue(pacman.myMap.move("pacman", new Location(7, 9), Map.Type.PACMAN));
-		
+		return null;
 	}
 }
-
