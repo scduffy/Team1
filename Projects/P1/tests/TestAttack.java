55f4e5397781d2e808f6d136d06568d73430ef27
AnthonyLiu-Git <anthonyliu2013@gmail.com>
2022-01-10 09:33:57 -0500

    Revert "Test case for Attack in Ghost Class"
    This reverts commit 22029820ec9a0adb11715907bd7bf82fcf57a74b.

--- Projects/P1/tests/TestAttack.java
+++ Projects/P1/tests/TestAttack.java
@@ -5,14 +5,6 @@ import java.io.*;
 public class TestAttack extends TestCase {
 
 	public void testAttack() throws FileNotFoundException{
-		MainFrame frame = new MainFrame();
-
-		Ghost ghost1 = frame.addGhost(new Location(1, 1), "ghost1", Color.red);
-		Ghost ghost2 = frame.addGhost(new Location(5, 9), "ghost2", Color.blue);
-
-		PacMan pacman = frame.addPacMan(new Location(6, 9));
-		assertTrue(ghost1.attack() == false);
-		assertTrue(ghost2.attack());
+		return null;	
 	}
 }
-
