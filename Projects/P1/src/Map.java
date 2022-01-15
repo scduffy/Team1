c7f075d2a9fa88e262529f48935c53068ed0b510
mikeko <geonhee.ko@gmail.com>
2022-01-15 17:15:56 -0500

    fixed map.attack

--- Projects/P1/src/Map.java
+++ Projects/P1/src/Map.java
@@ -9,18 +9,18 @@ public class Map{
 		PACMAN,
 		GHOST,
 		WALL,
-		COOKIE		
+		COOKIE
 	}
 
-	
+
 	private HashMap<Location, HashSet<Type>> field;
 	private boolean gameOver;
 	private int dim;
 
 	private HashMap<String, Location> locations;
-	private HashMap<String, JComponent> components; 
+	private HashMap<String, JComponent> components;
 	private HashSet<Type> emptySet;
-	private HashSet<Type> wallSet; 
+	private HashSet<Type> wallSet;
 
 	private int cookies = 0;
 
@@ -48,11 +48,11 @@ public class Map{
 	public int getCookies() {
 		return cookies;
 	}
-	
+
 	public boolean isGameOver() {
 		return gameOver;
 	}
-		
+
 	public boolean move(String name, Location loc, Type type) {
 		// update locations, components, and field
 		// use the setLocation method for the component to move it to the new location
@@ -67,7 +67,7 @@ public class Map{
 
 		return true;
 	}
-	
+
 	public HashSet<Type> getLoc(Location loc) {
 		//wallSet and emptySet will help you write this method
 		return emptySet;
@@ -85,20 +85,20 @@ public class Map{
 					ghostLocation.shift(-1, 0) };
 			for (Location l : moves) {
 				if (field.get(l) != null && field.get(l).contains(Map.Type.PACMAN)) {
-					gameOver = false;
-					return false;
+					gameOver = true;
+					return true;
 				}
 			}
 		}
-		return true;
+		return false;
 	}
-	
+
 	public JComponent eatCookie(String name) {
 		//update locations, components, field, and cookies
 		//the id for a cookie at (10, 1) is tok_x10_y1
 		Location loc = locations.get(name);
 		String cookie = "tok_x" + loc.x + "_y" + loc.y;
-		
+
 		if (locations.get(cookie) != null) {
 			locations.remove(cookie);
 			cookies--;
