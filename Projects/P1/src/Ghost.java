033be478c3f3ebbdf0f86b7eec6a03740d63a9e1
mikeko <geonhee.ko@gmail.com>
2022-01-15 17:33:56 -0500

    had fixed the wrong function

--- Projects/P1/src/Ghost.java
+++ Projects/P1/src/Ghost.java
@@ -45,7 +45,7 @@ public class Ghost{
 
 		for (Location l : movesAvailable) {
 			if (myMap.getLoc(l).contains(Map.Type.PACMAN))
-				return false;
+				return true;
 		}
 		return false;
 	}
@@ -57,7 +57,7 @@ public class Ghost{
 			ArrayList<Location> moves = get_valid_moves();
 
 			for (Location l : moves) {
-				if (myMap.getLoc(l) != null && myMap.getLoc(l).contains(Map.Type.PACMAN)) {
+				if (myMap.getLoc(l) == null && myMap.getLoc(l).contains(Map.Type.PACMAN)) {
 					myMap.attack(myName);
 					return true;
 				}
